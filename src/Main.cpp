c11225ed592c7415bc33b5ae54f6666d6ed978e5
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2023-02-06 15:34:03 +0200

    create library files

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "BasicFunctions.h"
+#include "src/BasicFunctions.h"
 #include <fstream>
 #include <sstream>
 #include <algorithm>
@@ -10,14 +10,6 @@ int main() {
 
 	int arr[] = { 2, 0, 1, 3, 1, 5, 4, 4, 5, 4, 23, 45, 78, 100, 32, 12, 12, 0, 3, 49 };
 	cout << BinarySearch(arr, 0, 19, 4);
-	/*
-	PrintArray(arr, 20);
-	//InsertionSort(arr, 7, descending);
-	HeapSort(arr, 20, ascending);
-	PrintArray(arr, 20);
-	*/
-	
-	
 	
 	return 0;
 }
